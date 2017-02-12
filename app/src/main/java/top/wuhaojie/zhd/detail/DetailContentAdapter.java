c3caad567ada69836082206448e308528ba29335
wuhaojie <w19961009@126.com>
2017-02-12 11:27:23 +0800

    init Detail Content.

---
+++ app/src/main/java/top/wuhaojie/zhd/detail/DetailContentAdapter.java
@@ -0,0 +1,26 @@
+package top.wuhaojie.zhd.detail;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by wuhaojie on 17-2-12.
+ */
+
+public class DetailContentAdapter extends FragmentStatePagerAdapter {
+
+    public DetailContentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
