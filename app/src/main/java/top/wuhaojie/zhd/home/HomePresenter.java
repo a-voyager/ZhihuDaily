f5359fec188509ae5dd1ad3de91575a2d1be7ce6
wuhaojie <w19961009@126.com>
2017-04-25 20:48:44 +0800

    nav updated.

--- app/src/main/java/top/wuhaojie/zhd/home/HomePresenter.java
+++ app/src/main/java/top/wuhaojie/zhd/home/HomePresenter.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import rx.Subscriber;
 import top.wuhaojie.zhd.R;
 import top.wuhaojie.zhd.base.interfaces.BasePresenter;
 import top.wuhaojie.zhd.base.interfaces.BaseView;
+import top.wuhaojie.zhd.data.HttpUtils;
+import top.wuhaojie.zhd.entities.ThemesListResponse;
 import top.wuhaojie.zhd.setting.SettingActivity;
 
 import static top.wuhaojie.zhd.home.HomeNavigationAdapter.*;
@@ -43,13 +47,38 @@ public class HomePresenter implements BasePresenter {
     }
 
     public void onCreate(Bundle savedInstanceState) {
-        ArrayList<Item> list = new ArrayList<>();
-        Item<Home> item = new Item<>(TYPE_HOME, null);
+
+        HttpUtils.getThemesList(new Subscriber<ThemesListResponse>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(ThemesListResponse response) {
+                handleResult(response);
+            }
+        });
+
+    }
+
+    private void handleResult(ThemesListResponse response) {
+        ArrayList<HomeNavigationAdapter.Item> list = new ArrayList<>();
+        HomeNavigationAdapter.Item<HomeNavigationAdapter.Home> item = new HomeNavigationAdapter.Item<>(TYPE_HOME, null);
         list.add(item);
 
-//        list.add(new Item<>(TYPE_OTHERS, new Others()));
-//        list.add(new Item<>(TYPE_OTHERS, new Others()));
-//        list.add(new Item<>(TYPE_OTHERS, new Others()));
+
+        List<ThemesListResponse.OthersBean> others = response.getOthers();
+        for (ThemesListResponse.OthersBean o : others) {
+            Others otherInItem = new Others(o.getColor(), o.getThumbnail(), o.getDescription(), o.getId(), o.getName());
+            Item<Others> oItem = new Item<>(TYPE_OTHERS, otherInItem);
+            list.add(oItem);
+        }
 
         mView.setNavAdapterList(list);
 
